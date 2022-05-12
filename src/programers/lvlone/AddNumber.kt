cdef75a5518c01cb7328b37698ab1c0efd437174
user <km_413@naver.com>
2022-05-12 15:32:12 +0900

    https://programmers.co.kr/learn/courses/30/lessons/86051?language=kotlin - 없는 숫자 더하기

---
+++ src/programers/lvlone/AddNumber.kt
@@ -0,0 +1,12 @@
+package programers.lvlone
+
+/*
+https://programmers.co.kr/learn/courses/30/lessons/86051?language=kotlin
+코딩테스트 연습
+월간 코드 챌린지 시즌3
+없는 숫자 더하기
+
+ */
+class SolutionAddNumber {
+    fun solution(numbers: IntArray): Int = 45 - numbers.sum()
+}
